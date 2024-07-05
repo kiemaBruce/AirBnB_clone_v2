6d81af330cebdac3b5fbf58d8294d442ebd2c1eb
kiemaBruce <brucekiema292@gmail.com>
2024-07-05 16:41:27 +0300

    Modify string printed with str()

--- models/state.py
+++ models/state.py
@@ -1,8 +1,25 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
 
 
-class State(BaseModel):
+class State(BaseModel, Base):
     """ State class """
     name = ""
+    __tablename__ = 'states'
+    name = Column(String(128), nullable=False)
+    cities = relationship("City", backref="state", cascade="all, delete")
+
+    @property
+    def cities(self):
+        """Returns the list of city instances with state_id equal to current
+        State.id"""
+        my_cities = []
+        from models.city import City
+        from models.__init__ import storage
+        for key, value in storage.all(City).items():
+            if value.state_id == State.id:
+                my_cities.append(value)
+        return my_cities
