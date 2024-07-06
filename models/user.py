d3bc88be2c66ea3d08a2d43208477625cd74a108
kiemaBruce <brucekiema292@gmail.com>
2024-07-06 23:59:11 +0300

    Create reviews relationship to relate with Review class
    If a User is deleted then all linked Review objects are automatically
    deleted as well.

--- models/user.py
+++ models/user.py
@@ -14,3 +14,5 @@ class User(BaseModel, Base):
     last_name = Column(String(128), nullable=False, server_default="NULL")
     places = relationship("Place", back_populates="user",
                           cascade="all, delete-orphan")
+    reviews = relationship("Review", back_populates="user",
+                           cascade="all, delete-orphan")
