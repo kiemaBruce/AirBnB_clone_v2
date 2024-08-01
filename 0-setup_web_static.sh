e5aea376bfaf6074a8a9c097633b29659d8c7e9b
kiemaBruce <brucekiema292@gmail.com>
2024-08-01 11:48:01 +0300

    Add sudo to within the script

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -4,15 +4,15 @@
 if ! command -v nginx > /dev/null
 then
 	# Install Nginx
-	apt-get update > /dev/null
-	apt-get install -y nginx > /dev/null
+	sudo apt-get update > /dev/null
+	sudo apt-get install -y nginx > /dev/null
 fi
 # Create directories if they don't exist
-mkdir /data/ > /dev/null 2>&1
-mkdir /data/web_static/ > /dev/null 2>&1
-mkdir /data/web_static/releases/ > /dev/null 2>&1
-mkdir /data/web_static/shared/ > /dev/null 2>&1
-mkdir /data/web_static/releases/test/ > /dev/null 2>&1
+sudo mkdir /data/ > /dev/null 2>&1
+sudo mkdir /data/web_static/ > /dev/null 2>&1
+sudo mkdir /data/web_static/releases/ > /dev/null 2>&1
+sudo mkdir /data/web_static/shared/ > /dev/null 2>&1
+sudo mkdir /data/web_static/releases/test/ > /dev/null 2>&1
 # Create simple html page
 html_content=$(cat <<EOL
 <html>
@@ -24,15 +24,15 @@ html_content=$(cat <<EOL
 </html>
 EOL
 )
-echo "$html_content" | tee /data/web_static/releases/test/index.html > /dev/null
+echo "$html_content" | sudo tee /data/web_static/releases/test/index.html > /dev/null
 # Create symbolic link
 if [ -L /data/web_static/current ]
 then
-	rm /data/web_static/current
+	sudo rm /data/web_static/current
 fi
-ln --symbolic /data/web_static/releases/test /data/web_static/current > /dev/null
+sudo ln --symbolic /data/web_static/releases/test /data/web_static/current > /dev/null
 # Give ownership of a directory to a specific user and group
-chown -R ubuntu:ubuntu /data/
+sudo chown -R ubuntu:ubuntu /data/
 rep_str=$(cat <<EOL
         server_name _;
 
@@ -43,11 +43,11 @@ EOL
 )
 echo "$rep_str" > temp_file
 awk '/server_name _;/ && !found {system("cat temp_file"); found=1; next} 1' /etc/nginx/sites-available/default > output_file
-rm temp_file
-mv output_file /etc/nginx/sites-available/default
+sudo rm temp_file
+sudo mv output_file /etc/nginx/sites-available/default
 #check for any errors with nginx config files
-if nginx -t &> /dev/null
+if sudo nginx -t &> /dev/null
 then
 	# reload nginx
-	nginx -s reload
+	sudo nginx -s reload
 fi
