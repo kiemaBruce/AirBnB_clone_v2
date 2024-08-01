af9389e2bce695e77fefaa083af53c532f091ecd
kiemaBruce <brucekiema292@gmail.com>
2024-08-01 10:44:48 +0300

    Remove 'sudo' from script

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -4,15 +4,15 @@
 if ! command -v nginx &> /dev/null
 then
 	# Install Nginx
-	sudo apt-get update -q
-	sudo apt-get install -qy nginx
+	apt-get update -q
+	apt-get install -qy nginx
 fi
 # Create directories if they don't exist
-sudo mkdir /data/
-sudo mkdir /data/web_static/
-sudo mkdir /data/web_static/releases/
-sudo mkdir /data/web_static/shared/
-sudo mkdir /data/web_static/releases/test/
+mkdir /data/
+mkdir /data/web_static/
+mkdir /data/web_static/releases/
+mkdir /data/web_static/shared/
+mkdir /data/web_static/releases/test/
 # Create simple html page
 html_content=$(cat <<EOL
 <html>
@@ -24,7 +24,7 @@ html_content=$(cat <<EOL
 </html>
 EOL
 )
-echo "$html_content" | sudo tee /data/web_static/releases/test/index.html > /dev/null
+echo "$html_content" | tee /data/web_static/releases/test/index.html > /dev/null
 # Create symbolic link
 if [ -L /data/web_static/current ]
 then
@@ -32,7 +32,7 @@ then
 fi
 ln --symbolic /data/web_static/releases/test /data/web_static/current
 # Give ownership of a directory to a specific user and group
-sudo chown -R ubuntu:ubuntu /data/
+chown -R ubuntu:ubuntu /data/
 rep_str=$(cat <<EOL
         server_name _;
 
@@ -43,13 +43,13 @@ EOL
 )
 echo "$rep_str" > temp_file
 awk '/\tserver_name _;/ && !found {system("cat temp_file"); found=1; next} 1' /etc/nginx/sites-available/default > output_file
-sudo rm temp_file
-sudo mv output_file /etc/nginx/sites-available/default
+rm temp_file
+mv output_file /etc/nginx/sites-available/default
 #check for any errors with nginx config files
-if sudo nginx -t
+if nginx -t
 then
 	# reload nginx
-	sudo nginx -s reload
+	nginx -s reload
 else
 	echo "Error with nginx config files"
 fi
