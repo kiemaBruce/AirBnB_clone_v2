3f955e9837d4ffeeb58cc41b7ea92f2b706b58c7
kiemaBruce <brucekiema292@gmail.com>
2024-08-07 16:07:26 +0300

    Create a .tgz archive from /web_static, store it in /versions

---
+++ 1-pack_web_static.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+# Generates a .tgz archive
+
+from datetime import datetime
+from fabric.api import *
+
+
+def do_pack():
+    """Creates a .tgz archive
+
+    Returns:
+        str: the archive path if the archive has been correctly generated,
+        otherwise it returns None
+    """
+    local('mkdir -p versions')
+    now = datetime.now()
+    archive_name = "web_static_"
+    archive_name = ("web_static_"
+                    + f"{now.year}"
+                    + f"{now.month}"
+                    + f"{now.day}"
+                    + f"{now.hour}"
+                    + f"{now.minute}"
+                    + f"{now.second}"
+                    + ".tgz"
+                    )
+    result = local(f'tar -zcvf ./versions/{archive_name} web_static')
+    if result.succeeded:
+        return archive_name
+    return None
