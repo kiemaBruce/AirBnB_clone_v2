3699110ce25ef80f6a8f99e0cc30dd223666ef1d
kiemaBruce <brucekiema292@gmail.com>
2024-08-08 12:01:32 +0300

    Only execute commands remotely, remove user definition in script

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -4,7 +4,6 @@
 from fabric.api import *
 
 
-env.user = 'ubuntu'
 env.hosts = ['54.234.93.82', '52.91.150.83']
 
 
@@ -28,29 +27,22 @@ def do_deploy(archive_path):
                     f"/data/web_static/releases/" +
                     f"{archive_name.split(sep='.')[0]}")
         run(f'sudo mkdir -p {dir_name}')
-        local(f'sudo mkdir -p {dir_name}')
         # extract
         run(f"sudo tar -xzf /tmp/{archive_name} -C {dir_name}")
-        local(f"sudo tar -xzf /tmp/{archive_name} -C {dir_name}")
         # delete archive
         run(f'sudo rm -f /tmp/{archive_name}')
-        local(f'sudo rm -f /tmp/{archive_name}')
         # move files to outer directory
         current = (
                 f"/data/web_static/releases/" +
                 f"{archive_name.split(sep='.')[0]}/web_static/*")
         new = f"/data/web_static/releases/{archive_name.split(sep='.')[0]}"
         run(f"sudo mv {current} {new}")
-        local(f"sudo mv {current} {new}")
         # delete the old (inner) directory
         run(f'sudo rm -rf {new}/web_static')
-        local(f'sudo rm -rf {new}/web_static')
         # delete old symbolic link
         run('sudo rm -f /data/web_static/current')
-        local('sudo rm -f /data/web_static/current')
         # create new symbolic link
         run(f'ln -s {dir_name} /data/web_static/current')
-        local(f'ln -s {dir_name} /data/web_static/current')
     except Exception as e:
         return False
     else:
