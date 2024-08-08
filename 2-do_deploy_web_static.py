bb08233ca6fc09f9ce37b61fd3f7524b159e186c
kiemaBruce <brucekiema292@gmail.com>
2024-08-08 11:53:28 +0300

    Execute commands both locally and remotely

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -28,22 +28,29 @@ def do_deploy(archive_path):
                     f"/data/web_static/releases/" +
                     f"{archive_name.split(sep='.')[0]}")
         run(f'sudo mkdir -p {dir_name}')
+        local(f'sudo mkdir -p {dir_name}')
         # extract
         run(f"sudo tar -xzf /tmp/{archive_name} -C {dir_name}")
+        local(f"sudo tar -xzf /tmp/{archive_name} -C {dir_name}")
         # delete archive
-        run(f'sudo rm -f /tmp/{archive_name}', warn_only=True)
+        run(f'sudo rm -f /tmp/{archive_name}')
+        local(f'sudo rm -f /tmp/{archive_name}')
         # move files to outer directory
         current = (
                 f"/data/web_static/releases/" +
                 f"{archive_name.split(sep='.')[0]}/web_static/*")
         new = f"/data/web_static/releases/{archive_name.split(sep='.')[0]}"
         run(f"sudo mv {current} {new}")
+        local(f"sudo mv {current} {new}")
         # delete the old (inner) directory
         run(f'sudo rm -rf {new}/web_static')
+        local(f'sudo rm -rf {new}/web_static')
         # delete old symbolic link
         run('sudo rm -f /data/web_static/current')
+        local('sudo rm -f /data/web_static/current')
         # create new symbolic link
         run(f'ln -s {dir_name} /data/web_static/current')
+        local(f'ln -s {dir_name} /data/web_static/current')
     except Exception as e:
         return False
     else:
