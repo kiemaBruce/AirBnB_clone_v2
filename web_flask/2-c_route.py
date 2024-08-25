9a617d3f210842ee6d6cb19a49c7e05e9e2e0eb7
kiemaBruce <brucekiema292@gmail.com>
2024-08-25 13:44:36 +0300

    Start a flask web app and define 3 routes

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+"""Starts a Flask web application, and defines  routes.
+"""
+
+from flask import Flask
+
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def root():
+    """Defines what happens when the root directory is accesed.
+    """
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """Defines what happens when the /hbnb directory is accessed.
+    """
+    return 'HBNB'
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def c_func(text):
+    """Defines what happens when the /c/<text> directory is accessed.
+    """
+    # First replace all underscores with space
+    for index, char in enumerate(text):
+        if char == '_':
+            my_str = text[:index] + ' ' + text[index + 1:]
+    return f'C {my_str}'
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
