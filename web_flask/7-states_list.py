dc94dae47aec6b23c026e3c4ef2e663c8a36abe5
kiemaBruce <brucekiema292@gmail.com>
2024-09-04 22:44:22 +0300

    List all states with a Flask web application
    These states are fetched from AirBnB app's db storage.

---
+++ web_flask/7-states_list.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""Starts a Flask web application, and defines routes.
+"""
+
+from flask import Flask
+from flask import render_template
+from models import storage
+from models.state import State
+
+
+app = Flask(__name__)
+
+
+@app.route('/states_list', strict_slashes=False)
+def display_states():
+    """Displays all States"""
+    states_dict = storage.all(State)
+    sorted_dict = sorted(states_dict.items(), key=lambda item: item[1].name)
+    return render_template('7-states_list.html', states=states_dict)
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
